8095fc6fc40b2d6f6ca49cab9ab859f38267d7c0
GustavooPerini <gustavooperini@gmail.com>
2025-07-18 00:22:22 -0300

    Criando um ambiente de desenvolvimento com docker. Ainda irei adicionar um README.md com um tutorial de como utilizar esse ambiente

---
+++ doefacil-backend/src/main/java/com/projeto/doe_facil/DoeFacilApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.doe_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoeFacilApplication.class, args);
+	}
+
+}
---
+++ doefacil-backend/src/test/java/com/projeto/doe_facil/DoeFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.doe_facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
